e78883a65b8dffc0c03300fd96434fde696305ba
Gaboelc <gabrielleon917@gmail.com>
2024-10-07 07:28:52 -0600

    Refactor main.py for improved tittles in terminal

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ def process_3dmark_files(raw_directory):
     for filename in os.listdir(raw_directory):
         if filename.endswith(".3dmark-result"):
             file_path = os.path.join(raw_directory, filename)
-            print(f"Processing file: {filename}")
+            print(f"""Processing file: 
+                  {filename}
+                  """)
 
             parser = ZipParser(file_path)
             temp_dir = parser.extract_to_temp()
 
             monitoring_file = os.path.join(temp_dir, 'Monitoring.csv')
             if os.path.exists(monitoring_file):
-                print(f"Analyzing {monitoring_file}")
                 output_file = os.path.join(temp_dir, 'Monitoring_cleaned.csv')
                 df = monitoring_etl(monitoring_file, output_file, threshold=0.3)
                 
@@ -22,12 +23,18 @@ def process_3dmark_files(raw_directory):
                     df_gpu, df_cpu = separate_and_order_columns(df)
 
                     cpu_validation_results = validate_cpu_data(df_cpu)
-                    print("CPU validation results:")
+                    print("\n CPU validation results:")
                     print(cpu_validation_results)
-
+                    
                     gpu_validation_results = validate_gpu_data(df_gpu)
-                    print("GPU validation results:")
+                    print("\n CPU validation results:")
                     print(gpu_validation_results)
+
+                    print("\n CPU Dataframe:")
+                    print(df_cpu)
+                    
+                    print("\n GPU Dataframe:")
+                    print(df_gpu)
             
             parser.clean_up()
 
